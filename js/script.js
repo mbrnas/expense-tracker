05be1324f399b9534e1cd7ff590e3cace8917b65
Matija Brnas <mb8828@rit.edu>
2023-05-25 23:27:04 +0200

    added pie chart

--- js/script.js
+++ js/script.js
@@ -1,24 +1,87 @@
 const transaction = document.getElementById("transaction");
 const category = document.getElementById("category");
 const date = document.getElementById("date");
-const formSubmit = document.getElementById("form-submit");
+const radios = document.getElementsByName("payment-type");
+const amount = document.getElementById("amount");
+const description = document.getElementById("description");
+
 const expenseForm = document.getElementById("expense-form");
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const transactionValue = transaction.value;
+  const categoryValue = category.value;
+  const dateValue = date.value;
+  let selectedPaymentType = null;
+
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      selectedPaymentType = radios[i].value;
+      break;
+    }
+  }
+
+  const amountValue = amount.value;
+  const descriptionValue = description.value;
+
+  const expense = {
+    transaction: transactionValue,
+    category: categoryValue,
+    date: dateValue,
+    paymentType: selectedPaymentType,
+    amount: amountValue,
+    description: descriptionValue,
+  };
+
+  console.log(expense);
+
+  expenseForm.reset();
+}
+
+expenseForm.addEventListener("submit", handleFormSubmit);
 
 
-function handleFormSubmit(event) { 
-    event.preventDefault();
-    const transactionValue = transaction.value;
+// Get a reference to the canvas element
+const pieChartCanvas = document.getElementById('pie-chart');
 
-    const categoryValue = category.value;
+// Function to create and update the pie chart
+function createPieChart(data) {
+  // Extract labels and amounts from the expense data
+  const labels = data.map(expense => expense.category);
+  const amounts = data.map(expense => expense.amount);
 
-    const dateValue = date.value;
+  // Create the chart using Chart.js
+  const pieChart = new Chart(pieChartCanvas, {
+    type: 'pie',
+    data: {
+      labels: labels,
+      datasets: [{
+        data: amounts,
+        backgroundColor: ['#2CD3E1', '#FF6384', '#36A2EB', '#FFCE56'], // Customize the colors as needed
+      }],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true, // Adjust as per your layout needs
+    },
+  });
+}
 
-    const radiosValue = radios.value;
+// Example expense data
+const expenses2 = [
+  {
+    category: 'Food',
+    amount: 100,
+  },
+  {
+    category: 'Transportation',
+    amount: 80,
+  },
+  {
+    category: 'Utilities',
+    amount: 75,
+  },
+];
 
-    console.log(transactionValue + ' ' + categoryValue + ' ' + dateValue + ' ' + radiosValue);
-
-    expenseForm.reset();
- }
-
-expenseForm.addEventListener('submit', handleFormSubmit);
+// Call the createPieChart function with the expense data
+createPieChart(expenses2);
